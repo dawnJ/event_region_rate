32f2b8a3a93208193c411cf4da75bfaa96c355d0
dawnJ <572113284@qq.com>
2014-11-05 11:10:45 +0800

    Revert "Revert "Revert "Revert "区域转移概率矩阵实验计算""""
    This reverts commit dac77f302247543e46b53f731d50b7659bd3b71d.

---
+++ event_regionid_rate/src/event_regionid_rate/main.java
@@ -0,0 +1,12 @@
+package event_regionid_rate;
+
+public class main {
+	
+	public static void main(String[] args) {
+		ReadFile rFile=new ReadFile();
+		//rFile.dealRegionNFile();
+		//rFile.dealSumFile();
+		rFile.dealRateFile();
+	}
+	
+}
